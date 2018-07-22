3e9ce30e8e6b66ab9b3733e0e4ab1f544009b769
sajin <sajins2005@gmail.com>
2018-07-22 15:56:18 +1000

    opencv AOT Video Matching Against Mpeg stream from camera2 API-Raspberry pi

--- app/src/main/java/com/example/sajin/aot_cam/MainActivity.kt
+++ app/src/main/java/com/example/sajin/aot_cam/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.sajin.aot_cam
 
-//import org.bytedeco.javacpp.BytePointe
-
 import android.Manifest
 import android.app.Activity
 import android.content.pm.PackageManager
@@ -46,14 +44,11 @@ class MainActivity : Activity(), LoaderCallbackInterface {
         Log.d(TAG, "Doorbell Activity created.")
 
         if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            // A problem occurred auto-granting the permission
             Log.e(TAG, "No permission")
             return
         }
-
         val mediaStorageDir = File(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_PICTURES), "CameraDemo")
-
         if (!mediaStorageDir.exists()) {
             !mediaStorageDir.mkdirs()
         }
@@ -63,35 +58,21 @@ class MainActivity : Activity(), LoaderCallbackInterface {
         mCameraThread!!.start()
         mCameraHandler = Handler(mCameraThread!!.looper)
         val button = button
-
-        button.setOnClickListener({
+        button.setOnClickListener{
             mCamera!!.takePicture()
-        })
-
-        //  sh.setFixedSize(480, 620)
+        }
         mCamera = CameraLib.instance
-        mCamera!!.initializeCamera(this, mCameraHandler!!, mOnImageAvailableListener, sh
-
-        )
-
-
-    }
-
-    private fun initPIO() {
-
-
+        mCamera!!.initializeCamera(this, mCameraHandler!!, mOnImageAvailableListener, sh)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         mCamera!!.shutDown()
-
         mCameraThread!!.quitSafely()
         try {
         } catch (e: IOException) {
             Log.e(TAG, "button driver error", e)
         }
-
     }
 
     override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
@@ -103,22 +84,16 @@ class MainActivity : Activity(), LoaderCallbackInterface {
         return super.onKeyUp(keyCode, event)
     }
 
-
     private val mOnImageAvailableListener = OnImageAvailableListener { reader ->
         val image = reader.acquireLatestImage()
-
-
         val imageBuf = image.planes[0].buffer
-        // var  buf: byte[]
         val imageBytes = ByteArray(imageBuf.remaining())
-
         imageBuf.get(imageBytes)
         var img1 = Mat(1, image.height * image.width, CvType.CV_8UC3, imageBuf)
         var img = Imgcodecs.imdecode(img1, Imgcodecs.CV_LOAD_IMAGE_UNCHANGED);
-
         image.close()
-        onPictureTaken(img)
 
+        onPictureTaken(img)
     }
 
     private fun onPictureTaken(ima: Mat) {
@@ -141,7 +116,6 @@ class MainActivity : Activity(), LoaderCallbackInterface {
 
     override fun onResume() {
         super.onResume()
-
         Log.i("DEMO", "Trying to load OpenCV library")
         if (!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mOpenCVCallBack)) {
             Log.e("DEMO", "Cannot connect to OpenCV Manager")
@@ -149,20 +123,16 @@ class MainActivity : Activity(), LoaderCallbackInterface {
     }
 
     protected fun onOpenCVReady() {
-
         Toast.makeText(applicationContext, "opencv ready", Toast.LENGTH_LONG).show()
-
     }
 
     override fun onManagerConnected(status: Int) {
         Log.e("DEMO", "connected" + Int)
-
     }
 
     override fun onPackageInstall(operation: Int,
                                   callback: InstallCallbackInterface) {
         // TODO Auto-generated method stub
-
     }
 
     public fun run(img: Mat, templateFile: String,
@@ -170,13 +140,12 @@ class MainActivity : Activity(), LoaderCallbackInterface {
         println("\nRunning Template Matching")
         var tt = Imgcodecs.imread(templateFile)
         //  var tt = Mat(templ.rows(),templ.cols(),CvType.CV_8U)
-          // Imgproc.Canny(templ,tt,50.0,200.0)
+        // Imgproc.Canny(templ,tt,50.0,200.0)
         //   // / Create the result matrix
         val result_cols = img.cols() - tt.cols() + 1
         val result_rows = img.rows() - tt.rows() + 1
         val result = Mat(result_rows, result_cols, CvType.CV_32FC1)
 
-        // / Do the Matching and Normalize
         Imgproc.matchTemplate(img, tt, result, match_method)
 
         Core.normalize(result, result, 0.0, 1.0, Core.NORM_MINMAX, -1, Mat())
@@ -190,13 +159,12 @@ class MainActivity : Activity(), LoaderCallbackInterface {
         } else {
             matchLoc = mmr.maxLoc
         }
+
         loop@ while (true) {
             Log.e("DEMO", "__________")
             mmr = Core.minMaxLoc(result)
             maxval = mmr.maxVal
             matchLoc = mmr.maxLoc
-            // dst = img.clone();
-            // / Show me what you got
             if (maxval > .99) {
                 Imgproc.rectangle(img, matchLoc, Point(matchLoc.x + tt.cols(),
                         matchLoc.y + tt.rows()), Scalar(0.0, 255.0, 0.0))
@@ -205,21 +173,18 @@ class MainActivity : Activity(), LoaderCallbackInterface {
             } else {
                 break@loop
             }
-
         }
-        //println("Writing $outFile")
+
         Imgcodecs.imwrite(pt + "kk3.png", img)
         Imgcodecs.imwrite(pt + "kk2.png", tt)
-        // val return_buff = ByteArray((img.total() * img.channels()) as Int)
         val bm = Bitmap.createBitmap(img.cols(), img.rows(), Bitmap.Config.ARGB_8888)
         Utils.matToBitmap(img, bm)
         val bMap = bm
-        runOnUiThread({
-
+        runOnUiThread {
             var canvs = sh.lockCanvas()
             canvs.drawBitmap(bMap, 0F, 0F, null)
             sh.unlockCanvasAndPost(canvs)
-        })
+        }
     }
 
 
@@ -230,11 +195,9 @@ class MainActivity : Activity(), LoaderCallbackInterface {
         val width = image.getWidth()
         val height = image.getHeight()
         var offset = 0
-
         val planes = image.getPlanes()
         val data = ByteArray(image.getWidth() * image.getHeight() * ImageFormat.getBitsPerPixel(ImageFormat.YUV_420_888) / 8)
         val rowData = ByteArray(planes[0].getRowStride())
-
         for (i in planes.indices) {
             buffer = planes[i].getBuffer()
             rowStride = planes[i].getRowStride()
@@ -263,13 +226,9 @@ class MainActivity : Activity(), LoaderCallbackInterface {
                 }
             }
         }
-
-        // Finally, create the Mat.
         val mat = Mat(height + height / 2, width, CvType.CV_8UC3)
         mat.put(0, 0, data)
-
         return mat
     }
-
 }
 
